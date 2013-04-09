a649f1b98b403e7bb17bb2808bd9105210dcab9d
maxconway <conway.max1@gmail.com>
2013-04-09 18:42:35 +0100

    add difficulty code
    -query bitcoind to find difficulty
    -placeholdrs for other constants

--- analysis1.R
+++ analysis1.R
@@ -1,5 +1,15 @@
+#const
+coinsperblock=50
+costperhash=0
+difficulty = system('bitcoind getdifficulty',intern=TRUE,)
+tryCatch(difficulty=as.numeric(difficult),stop('difficult should be a number'))
+assert(difficult!=0)
+costpercoin=difficulty*2^32*costperhash/coinsperhash
+
 trades <- read.csv("~/git/bitcoin/trades.csv?symbol=mtgoxUSD", header=F)#maybe this should wget directly
 colnames(trades)=c('time','price','amount')
+
+
 ## TODO:
 #compare to hashrate
 #look for arbitrage
