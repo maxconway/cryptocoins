781029e97cbca8dc8dc9b717484913bbd0acf0f3
maxconway <conway.max1@gmail.com>
2014-04-05 12:07:27 +0100

    tests for orders2igraph

---
+++ tests/testthat/test-apiclient.R
@@ -0,0 +1,23 @@
+context('test api')
+
+orders <- data.frame(
+  asset  = c('A',  'A',   'B',  'B',   'C',  'C'   ),
+  unit   = c('B',  'B',   'C',  'C',   'A',  'A'   ),
+  type   = c('buy','sell','buy','sell','buy','sell'),
+  price  = c(0.9,  1.1,   0.9,  1.1,   0.9,  1.1   ),
+  volume = 1
+)
+
+test_that('orders2igraph produces expected results', {
+  graph <- orders2igraph(orders)
+  
+  expect_that(graph['B','C',attr='rate'], equals(1.1))
+  expect_that(graph['C','B',attr='rate'], equals(1/0.9))
+})
+
+test_that('orders2igraph fees work correctly', {
+  graph_free <- orders2igraph(orders)
+  graph_fee <- orders2igraph(orders,buyfee=0.01,sellfee=0.01)
+  
+  expect_that(min(E(graph_free)$rate - E(graph_fee)$rate), is_more_than(0))
+})
