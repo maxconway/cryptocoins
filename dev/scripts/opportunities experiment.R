4c236f2b505c08ce1f2bf783146c70c2546c7afe
maxconway <conway.max1@gmail.com>
2014-04-05 23:23:55 +0100

    added bitrex, more elegant adding of own wallets

--- dev/scripts/opportunities experiment.R
+++ dev/scripts/opportunities experiment.R
@@ -1,36 +1,31 @@
 while(TRUE){
   try({
     orders_bter <- getorders_bter()
-    graph_bter <- orders2igraph(orders_bter,exchangename='bter')
+    graph_bter <- orders2igraph(orders_bter,0.2,0.2,exchangename='bter')
     
     orders_comkort <- getorders_comkort()
-    graph_comkort <- orders2igraph(orders_comkort,exchangename='comkort')
+    graph_comkort <- orders2igraph(orders_comkort,0.2,0.2,exchangename='comkort')
     
     orders_cryptsy <- getorders_cryptsy()
-    graph_cryptsy <- orders2igraph(orders_cryptsy,0.002,0.003,exchangename='cryptsy')
+    graph_cryptsy <- orders2igraph(orders_cryptsy,0.2,0.3,exchangename='cryptsy')
     
-    gr2 <- graph_bter %du% graph_cryptsy %du% graph_comkort
+    orders_bitrex <- getorders_bitrex()
+    graph_bitrex <- orders2igraph(orders_bitrex,exchangename='bitrex')
     
-    gr2 <- gr2 + edge('DOGE_cryptsy','DOGE_bter',rate=1,volume=100)
-    gr2 <- gr2 + edge('LTC_cryptsy','LTC_bter',rate=1,volume=10)
-    gr2 <- gr2 + edge('BTC_cryptsy','BTC_bter',rate=1,volume=10)
-    gr2 <- gr2 + edge('DOGE_bter','DOGE_cryptsy',rate=1,volume=100)
-    gr2 <- gr2 + edge('LTC_bter','LTC_cryptsy',rate=1,volume=10)
-    gr2 <- gr2 + edge('BTC_bter','BTC_cryptsy',rate=1,volume=10)
-    gr2 <- gr2 + edge('DOGE_bter','DOGE_comkort',rate=1,volume=100)
-    gr2 <- gr2 + edge('LTC_bter','LTC_comkort',rate=1,volume=10)
-    gr2 <- gr2 + edge('BTC_bter','BTC_comkort',rate=1,volume=10)
-    gr2 <- gr2 + edge('DOGE_comkort','DOGE_bter',rate=1,volume=100)
-    gr2 <- gr2 + edge('LTC_comkort','LTC_bter',rate=1,volume=10)
-    gr2 <- gr2 + edge('BTC_comkort','BTC_bter',rate=1,volume=10)
-    gr2 <- gr2 + edge('DOGE_cryptsy','DOGE_comkort',rate=1,volume=100)
-    gr2 <- gr2 + edge('LTC_cryptsy','LTC_comkort',rate=1,volume=10)
-    gr2 <- gr2 + edge('BTC_cryptsy','BTC_comkort',rate=1,volume=10)
-    gr2 <- gr2 + edge('DOGE_comkort','DOGE_cryptsy',rate=1,volume=100)
-    gr2 <- gr2 + edge('LTC_comkort','LTC_cryptsy',rate=1,volume=10)
-    gr2 <- gr2 + edge('BTC_comkort','BTC_cryptsy',rate=1,volume=10)
+    gr2 <- graph_bter %du% graph_cryptsy %du% graph_comkort %du% graph_bitrex
     
-    res <- optimize(augmentgraph(gr2,vertex='BTC_cryptsy'))
+    gr2 <- gr2 + vertices(c('BTC_wallet','LTC_wallet','DOGE_wallet'))
+    
+    markets <- c('bter','comkort','cryptsy','bitrex')
+    
+    for(currency in c('BTC','LTC','DOGE')){
+      gr2[from=rep_len(paste(sep='_',currency,'wallet'), length(markets)), to=paste(sep='_', currency, markets), attr='rate'] <- 1
+      gr2[from=rep_len(paste(sep='_',currency,'wallet'), length(markets)), to=paste(sep='_', currency, markets), attr='volume'] <- Inf
+      gr2[to=rep_len(paste(sep='_',currency,'wallet'), length(markets)), from=paste(sep='_', currency, markets), attr='rate'] <- 1
+      gr2[to=rep_len(paste(sep='_',currency,'wallet'), length(markets)), from=paste(sep='_', currency, markets), attr='volume'] <- Inf
+    }
+    
+    res <- optimize(augmentgraph(gr2,vertex='BTC_wallet'))
     
     write.table(data.frame(now(),res$optimum),
                 file='./dev/logs/opportunities.txt',
